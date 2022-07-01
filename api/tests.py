6bbc47d57c5920e7428d2ea4335046a70b3b32a7
almas <almas.giniatullin5@gmail.com>
2022-07-01 17:09:46 +0500

    test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from utilities import fraud_detector, service_classifier
 
-# Create your tests here.
+
+class UtilsTestClass(TestCase):
+
+    def test_fraud_detector(self):
+        res = fraud_detector.fraud_detector('test')
+        self.assertGreaterEqual(res, 0)
+        self.assertLessEqual(res, 1)
+
+    def test_service_classifier(self):
+        res = service_classifier.service_classifier('test')
+        self.assertIsInstance(res, dict)
